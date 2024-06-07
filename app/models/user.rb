997369d2b55f1469d0074b0d1c9e26e4695e199b
ren <raenierquiambao@gmail.com>
2024-06-07 14:18:48 +0800

    Fix number of Attended events on User show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
 
   has_many :attendances
-  has_many :attended_events, through: :attendances, source: :event
+  has_many :attended_events, -> { distinct }, through: :attendances, source: :event
 
   def full_name
     "#{first_name} #{second_name}"
